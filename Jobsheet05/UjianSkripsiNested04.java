75ddba328fe061a47eca2cac237109aa7407f3be
Nanda <anandafriska0907@gmail.com>
2025-10-10 07:38:04 +0700

    Percobaan 3

---
+++ Jobsheet05/UjianSkripsiNested04.java
@@ -0,0 +1,37 @@
+package Jobsheet05;
+import java.util.Scanner;
+
+public class UjianSkripsiNested04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String bebasKompen;
+        int bimbinganP1, bimbinganP2;
+        String pesan;
+
+        System.out.println("\nApakah mahasiswa sudah bebas kompen? (Ya/Tidak): ");
+        bebasKompen = sc.next();
+
+        System.out.print("Masukkan jumlah log bimbingan Pembimbing 1: ");
+        bimbinganP1 = sc.nextInt();
+
+        System.out.print("Masukkan jumlah log bimbingan Pembimbing 2: ");
+        bimbinganP2 = sc.nextInt();
+
+        if (bebasKompen.equalsIgnoreCase("Ya")) {
+            if (bimbinganP1 >= 8 && bimbinganP2 >= 4) {
+                pesan = "Semua syarat terpenuhi. Mahasiswa boleh mendaftar ujian skripsi";
+            } else if (bimbinganP1 < 8 && bimbinganP2 < 4) {
+                pesan = "Gagal! Log bimbingan P1 kurang dari 8 kali dan P2 kurang dari 4 kali";
+            } else if (bimbinganP1 < 8) {
+                pesan = "Gagal! Log bimbingan P1 belum mencapai 8 kali";
+            } else {
+                pesan = "Gagal! Log bimbingan P2 belum mencapai 4 kali";
+            }
+        } else {
+            pesan = "Gagal! Mahasiswa masih memiliki tanggungan kompen";
+        }
+
+        System.out.println(pesan);
+    }
+}
