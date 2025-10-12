e28e2082d2cf02575018eef309d66d988594daf9
Nanda <anandafriska0907@gmail.com>
2025-10-13 06:07:38 +0700

    Tugas 3

---
+++ Jobsheet05/Tugas304.java
@@ -0,0 +1,35 @@
+package Jobsheet05;
+
+import java.util.Scanner;
+
+public class Tugas304 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A, B, C;
+        System.out.print("\nMasukkan bilangan A: ");
+        A = sc.nextInt();
+        System.out.print("Masukkan bilangan B: ");
+        B = sc.nextInt();
+        System.out.print("Masukkan bilangan C: ");
+        C = sc.nextInt();
+
+        // Menentukan bilangan maksimum
+        if (A >= B && A >= C) {
+            System.out.println("Bilangan Maksimum = A (" + A + ")");
+        } else if (B >= A && B >= C) {
+            System.out.println("Bilangan Maksimum = B (" + B + ")");
+        } else {
+            System.out.println("Bilangan Maksimum = C (" + C + ")");
+        }
+
+        // Menentukan bilangan minimum
+        if (A <= B && A <= C) {
+            System.out.println("Bilangan Minimum = A (" + A + ")");
+        } else if (B <= A && B <= C) {
+            System.out.println("Bilangan Minimum = B (" + B + ")");
+        } else {
+            System.out.println("Bilangan Minimum = C (" + C + ")");
+        }
+    }
+}
