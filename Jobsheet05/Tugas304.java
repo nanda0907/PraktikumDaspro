8428580f1576eaeea889c0bc8b48b6a1ff0ea616
Nanda <anandafriska0907@gmail.com>
2025-10-13 17:18:12 +0700

    Modifikasi Tugas 3

--- Jobsheet05/Tugas304.java
+++ Jobsheet05/Tugas304.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Tugas304 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
         int A, B, C;
         System.out.print("\nMasukkan bilangan A: ");
         A = sc.nextInt();
@@ -15,18 +15,26 @@ public class Tugas304 {
         C = sc.nextInt();
 
         // Menentukan bilangan maksimum
-        if (A >= B && A >= C) {
-            System.out.println("Bilangan Maksimum = A (" + A + ")");
-        } else if (B >= A && B >= C) {
+        if (A > B) {
+            if (A > C) {
+                System.out.println("Bilangan Maksimum = A (" + A + ")");
+            } else {
+                System.out.println("Bilangan Maksimum = C (" + C + ")");
+            }
+        } else if (B > C) {
             System.out.println("Bilangan Maksimum = B (" + B + ")");
         } else {
             System.out.println("Bilangan Maksimum = C (" + C + ")");
         }
 
         // Menentukan bilangan minimum
-        if (A <= B && A <= C) {
-            System.out.println("Bilangan Minimum = A (" + A + ")");
-        } else if (B <= A && B <= C) {
+        if (A < B) {
+            if (A < C) {
+                System.out.println("Bilangan Minimum = A (" + A + ")");
+            } else {
+                System.out.println("Bilangan Minimum = C (" + C + ")");
+            }
+        } else if (B < C) {
             System.out.println("Bilangan Minimum = B (" + B + ")");
         } else {
             System.out.println("Bilangan Minimum = C (" + C + ")");
