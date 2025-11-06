d3d101c464451a71199733434de0332f69762b5c
Nanda <anandafriska0907@gmail.com>
2025-11-06 13:58:12 +0700

    Modif percobaan 4 menggunakan perulangan for

--- Jobsheet08/NilaiKelompok04.java
+++ Jobsheet08/NilaiKelompok04.java
@@ -1,16 +1,15 @@
 package Jobsheet08;
 
 import java.util.Scanner;
+
 public class NilaiKelompok04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int i = 1;
         double rataTertinggi = 0;
         int kelompokTertinggi = 0;
 
-
-        while (i <= 6) {
+        for (int i = 1; i <= 6; i++) {
             System.out.println("\nKelompok " + i);
             double totalNilai = 0;
 
@@ -19,17 +18,17 @@ public class NilaiKelompok04 {
                 double nilai = sc.nextDouble();
                 totalNilai += nilai;
             }
-            
+
             double rataNilai = totalNilai / 5;
             System.out.println("Rata-rata: " + rataNilai);
-
+            
             if (rataNilai > rataTertinggi) {
                 rataTertinggi = rataNilai;
                 kelompokTertinggi = i;
             }
-            i++;
         }
-        System.out.println("Kelompok dengan rata-rata tertinggi adalah kelompok " + kelompokTertinggi);
+
+        System.out.println("\nKelompok dengan rata-rata tertinggi adalah kelompok " + kelompokTertinggi);
         System.out.println("Rata-rata tertinggi: " + rataTertinggi);
     }
 }
