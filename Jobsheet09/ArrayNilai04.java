e579ec865d4a4bfba7cad2b433dc8e4aab2ae2f2
Nanda <anandafriska0907@gmail.com>
2025-11-13 20:41:07 +0700

    Modif percobaan 2

--- Jobsheet09/ArrayNilai04.java
+++ Jobsheet09/ArrayNilai04.java
@@ -13,7 +13,11 @@ public class ArrayNilai04 {
         }
 
         for (int i = 0; i < 10; i++){
-            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            }else {
+                System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
+            }
         }
     }
 }
